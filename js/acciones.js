37add900ce7c6c5cd6289575469987a025b607b1
AL03F13 <aponteeric95@gmail.com>
2015-12-03 12:59:00 -0600

    uno
    todo

---
+++ js/acciones.js
@@ -0,0 +1,30 @@
+// JavaScript Document
+$(document).ready(function(e) {
+document.addEventListener("deviceready",function(){
+	$('#botonmenu').on('tap',function(){
+	navigator.vibrate(1000);
+});
+$('#A').on('tap',function(){
+	navigator.vibrate(1000);
+});
+$('#B').on('tap',function(){
+	navigator.vibrate(1000);
+});
+$('#C').on('tap',function(){
+	navigator.vibrate(1000);
+});
+$('#D').on('tap',function(){
+	navigator.vibrate(1000);
+});
+$('#E').on('tap',function(){
+	navigator.vibrate(1000);
+});
+$('#F').on('tap',function(){
+	navigator.vibrate(1000);
+});
+
+}); 
+});
+
+
+
